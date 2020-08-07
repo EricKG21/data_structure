dc38e2620959597178d24d7b850ba13e09df65e8
bu <18200299727@163.com>
2020-08-06 21:29:43 -0700

    LC3

--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,20 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
+function lengthOfLongestSubstring(s) {
+    let windowCharsMap = {};
+    let startChar = 0;
+    let maxLength =0;
+    for (i = 0; i < s.length; i++ ){
+        let endChar = s[i];
+        if(windowCharsMap[endChar] >= startChar){
+            startChar = windowCharsMap[endChar] + 1;
+        }
+        windowCharsMap[endChar] = i;
+        maxLength = Math.max(maxLength, i - startChar + 1);
+
+    }
+    return maxLength
+}
 
 module.exports = lengthOfLongestSubstring;
